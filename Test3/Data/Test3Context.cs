4ed6bc871a6980a45a84da9406c609bc4a2408b5
TuanPhong20 <phongpika19@gmail.com>
2024-05-22 18:58:28 +0700

    Add project files.

---
+++ Test3/Data/Test3Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MvcMovie.Models;
+
+namespace Test3.Data
+{
+    public class Test3Context : DbContext
+    {
+        public Test3Context (DbContextOptions<Test3Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MvcMovie.Models.Movie> Movie { get; set; } = default!;
+    }
+}
---
+++ Test3/Models/Movie.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MvcMovie.Models;
+
+public class Movie
+{
+    [Display(Name = "Mã phim")]
+    public int Id { get; set; }
+
+    [StringLength(60, MinimumLength = 3)]
+    [Required]
+    [Display(Name = "Tên phim")]
+    public string? Title { get; set; }
+
+    [Display(Name = "Ngày phát hành")]
+    [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+    [DataType(DataType.Date)]
+    public DateTime ReleaseDate { get; set; }
+
+    [Range(1, 100)]
+    [DataType(DataType.Currency)]
+    [Column(TypeName = "decimal(18, 2)")]
+    [Display(Name = "Giá")]
+    public decimal Price { get; set; }
+
+    [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$")]
+    [Required]
+    [StringLength(30)]
+    [Display(Name = "Thể loại")]
+    public string? Genre { get; set; }
+
+    [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$")]
+    [StringLength(5)]
+    [Required]
+    public string? Rating { get; set; }
+}
